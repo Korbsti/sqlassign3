5414a8db5664f19499131102bcc4148dc232e29d
propatzer <114686326+propatzer@users.noreply.github.com>
2022-11-25 16:33:33 -0500

    Add files via upload

---
+++ 5minutes2hours.sql
@@ -0,0 +1,47 @@
+USE userInformation;
+INSERT INTO 
+	my_contacts ( 
+		`last_name`,`first_name`,`email`,`gender`,`birthday`,`profession`,`postal_code`,`status`,`attributes`,`friends`
+	)
+VALUES
+	('White','Walter','thereal_heisenberg@protonmail.com','M','1969-03-03','Chemistry Teacher','Alberquerque 87101','divorced','narcissistic;egoist;cunning;resourceful','Tyler Durden'),
+	('Bateman','Patrick','hueylewisenjoyer@gmail.com','M','1972-03-03','Businessman','New York 10101','single','narcissistic;cunning;resourceful;insane','Tyler Durden'),
+    ('Durden','Tyler','soapbox@gmail.com','M','1978-03-03','Soap Dealer','Delaware 10101','single','freespirited;extroverted;destructive','Walter White'),
+    ('Bendoza','Angelina','angelina@starbuzzcoffee.com','F','1979-08-19','UNIX Sysadmin','Angus, ON L0M','married','Self sufficient;Empathetic;Smart','Donald Murphy'),
+	('Bob','Dylan','bobbied@protonmail.com','M','1973-06-07','Singer','Alberquerque 87101','divorced','narcissistic;egoist;cunning','Tyler Durden'),
+    ('Green','Jodie','jodiegreen@protonmail.com','F','1990-06-08','Accountant','Sunderland L0C1H0','divorced','nice;controlling','Tyler Durden'),
+    ('Wilson','Matthew','matwil@gmail.com','M','1987-11-07','Arduino Professor','Barrie','married','knowledgeable;happy','Tyler Durden'),
+    ('Hutchinson','George','ghutchinson@gmail.com','M','1980-06-07','Math Professor','Guelph','engaged','smart;helpful;happy','Walter White'),
+    ('Smith','John','Jsmith@protonmail.com','M','1992-06-07','Student','Brock L0C','single','narcissistic;egoist;cunning;resourceful','Walter White'),
+    ('Donahuge','Emma','edwrites@protonmail.com','F','1979-01-11','Writer','Alberquerque 87101','single','creative;introverted','Patrick Bateman');
+    
+    INSERT INTO 
+	my_contacts ( 
+		`last_name`,`first_name`,`email`,`gender`,`birthday`,`profession`,`postal_code`,`status`,`attributes`,`friends`
+	)
+VALUES
+	('White','Walter','thereal_heisenberg@protonmail.com','M','1969-03-03','Chemistry Teacher','Alberquerque 87101','divorced','narcissistic;egoist;cunning;resourceful','Tyler Durden'),
+	('Bateman','Patrick','hueylewisenjoyer@gmail.com','M','1972-03-03','Businessman','New York 10101','single','narcissistic;cunning;resourceful;insane','Tyler Durden'),
+    ('Durden','Tyler','soapbox@gmail.com','M','1978-03-03','Soap Dealer','Delaware 10101','single','freespirited;extroverted;destructive','Walter White'),
+    ('Bendoza','Angelina','angelina@starbuzzcoffee.com','F','1979-08-19','UNIX Sysadmin','Angus, ON L0M','married','Self sufficient;Empathetic;Smart','Donald Murphy'),
+	('Bob','Dylan','bobbied@protonmail.com','M','1973-06-07','Singer','Alberquerque 87101','divorced','narcissistic;egoist;cunning','Tyler Durden'),
+    ('Green','Jodie','jodiegreen@protonmail.com','F','1990-06-08','Accountant','Sunderland L0C1H0','divorced','nice;controlling','Tyler Durden'),
+    ('Wilson','Matthew','matwil@gmail.com','M','1987-11-07','Arduino Professor','Barrie','married','knowledgeable;happy','Tyler Durden'),
+    ('Hutchinson','George','ghutchinson@gmail.com','M','1980-06-07','Math Professor','Guelph','engaged','smart;helpful;happy','Walter White'),
+    ('Smith','John','Jsmith@protonmail.com','M','1992-06-07','Student','Brock L0C','single','narcissistic;egoist;cunning;resourceful','Walter White'),
+    ('Donahuge','Emma','edwrites@protonmail.com','F','1979-01-11','Writer','Alberquerque 87101','single','creative;introverted','Patrick Bateman');
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
